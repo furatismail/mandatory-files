acace4ce6e49ce377aeb58b0eac080f0d6716a16
Furát Ismail <ismail.furat@gmail.com>
2022-06-16 08:16:07 +0200

    Add files via upload

---
+++ injector.ts
@@ -0,0 +1,32 @@
+class UserService {
+    sayHi() {
+        console.log("hi");
+    }
+}
+
+
+class Component {
+    constructor(public user: UserService) {}
+}
+
+class Injector {
+    private _container = new Map();
+
+    constructor(private _providers: any[] = []) {
+        this._providers.forEach(service => this._container.set(service, new service()))
+    }
+
+    get(service: any) {
+        const serviceInstance = this._container.get(service);
+        if(!serviceInstance) {
+            throw Error('No provider');
+        }
+
+        return serviceInstance
+    }
+}
+
+
+const injector = new Injector([UserService])
+const component = new Component(injector.get(UserService))
+component.user.sayHi()
