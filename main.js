f019c270df557dfca33b6e8d2479e1a192a20962
Furát Ismail <ismail.furat@gmail.com>
2022-09-27 08:42:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-  loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+// funkce umi nacist script z internetu
+function loadScript(src, callback) {
+    // vytvatrim html element script
+    let script = document.createElement('script');
+    // do script nacitam cestu na dany script - www.nekdekdesenachazitenscript.cz
+    script.src = src;
+    // jakmile se nacte tak volam callback
+    script.onload = () => callback(script);
+    // pripojuju element script do hlavicky
+    document.head.append(script);
+}
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
     alert(`Cool, the script ${script.src} is loaded`);
   });
+
+  
