16ae6803a1f73640694d1402c62d4456b653a7b8
Furát Ismail <ismail.furat@gmail.com>
2022-09-27 11:30:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-function getData(url) {
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-        .catch(function (error) {
-            console.log('Nastal error');
-            console.log(error);
-        })
-}
+//instance eventSource
+const evtSource = new EventSource('http://localhost:3000/event');
+const eventList = document.querySelector('ul');
+const button = document.querySelector('button');
 
-getData("https://jsonplaceholder.typicode.com/posts");
+console.log('readyState: ', evtSource.readyState);
+console.log('url: ', evtSource.url);
+
+evtSource.onopen = () => {
+  console.log("Connection to server opened.");
+};
+
+evtSource.onmessage = (e) => {
+  console.log('message', e)
+  const newElement = document.createElement("li");
+  newElement.textContent = "message: " + e.data;
+  eventList.appendChild(newElement);
+};
+
+evtSource.onerror = () => {
+  console.log("EventSource failed.");
+};
+
+button.onclick = () => {
+  console.log('Connection closed');
+  evtSource.close();
+};
