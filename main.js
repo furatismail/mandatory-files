e2f6b437a578ad32154d5c71b9c4a146916988b9
Furát Ismail <ismail.furat@gmail.com>
2022-09-27 09:21:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 function getData(url) {
-  let xhr = new XMLHttpRequest();
-  // budes volat metodu GET a dane url, ktere dostanes
-  xhr.open("GET", url);
-  // to co vratis, mi vratis v JSON
-  xhr.responseType = "json";
-
-  // pokud ti server vrati data tak je vykonzoluj
-  xhr.onload = function () {
-    console.log(xhr.response);
-  };
-
-  // pokud by server spadnul tak vykonzoluj error
-  xhr.onerror = function () {
-    console.log('Nastal error');
-  }
-
-  // zavolej 
-  xhr.send();
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+        .catch(function (error) {
+            console.log('Nastal error');
+            console.log(error);
+        })
 }
-
 
 getData("https://jsonplaceholder.typicode.com/posts");
