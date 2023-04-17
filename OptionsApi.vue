c314bdb86d57314542000b9fb841e086500682e0
Furát Ismail <ismail.furat@gmail.com>
2023-04-17 15:41:11 +0200

    Create OptionsApi.vue

---
+++ OptionsApi.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: "Options API"
+            }
+        },
+        methods: {
+            greet() {
+                console.log(this.title)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
