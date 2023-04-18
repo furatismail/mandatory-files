ff8a81b8d360c40168cd0bd4499cc5ebb9923e7d
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 09:05:53 +0200

    Add files via upload

---
+++ CompositionApi.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        const title = 'Composition API';
+
+        function greet() {
+            console.log(this.title);
+        }
+
+        return {
+            title,
+            greet
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ CompositionApiScript.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script setup>
+  const title = 'Composition API';
+
+  function greet() {
+        console.log(this.title);
+  }
+</script>
+
+<style scoped>
+
+</style>
