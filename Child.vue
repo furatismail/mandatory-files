0ddf961b888036a9c6941ad555733486f4ecd091
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 10:54:47 +0200

    Add files via upload

---
+++ Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        Potomek: {{ id }} {{ film }}
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    id: String,
+    film: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid #000;
+    background-color: yellow;
+    padding: 20px;
+    margin: 10px;
+}
+</style>
---
+++ Parent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="review in reviews">{{ review.film }}</div>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+    const reviews = reactive(
+        [
+            {id: "x", film: "Alien vs Predator"},
+            {id: "xx", film: "Alien vs Predator II"},
+            {id: "xxx", film: "Alien vs Predator III"},
+        ]
+    )
+</script>
+
+<style scoped>
+
+</style>
