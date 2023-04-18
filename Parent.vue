3c4e45380544d791c82905d1d09ad1c2fd61d450
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 11:21:01 +0200

    Update Parent.vue

--- Parent.vue
+++ Parent.vue
@@ -1,21 +1,29 @@
 <template>
-    <div>
-        <div v-for="review in reviews">{{ review.film }}</div>
+  <div>
+    <div v-for="review in reviews">
+      <Child :id="review.id" :film="review.film" @addFilm="updateCart($event)"></Child>
     </div>
+
+    Kosik: {{ chosenFilms }}
+  </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from 'vue'
+import Child from './Child.vue'
 
-    const reviews = reactive(
-        [
-            {id: "x", film: "Alien vs Predator"},
-            {id: "xx", film: "Alien vs Predator II"},
-            {id: "xxx", film: "Alien vs Predator III"},
-        ]
-    )
+const reviews = reactive([
+  { id: 'x', film: 'Alien vs Predator' },
+  { id: 'xx', film: 'Alien vs Predator II' },
+  { id: 'xxx', film: 'Alien vs Predator III' }
+])
+
+const chosenFilms = reactive([])
+
+function updateCart($event) {
+  console.log('Prichozi $event', event)
+  chosenFilms.push($event)
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
