d64c58a181382bbc66d1289e70186c804d6e21ef
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 10:07:25 +0200

    Update CompositionApiScript.vue

--- CompositionApiScript.vue
+++ CompositionApiScript.vue
@@ -2,15 +2,33 @@
   <div>
     {{ title }}
 
+    obj1: {{ obj }}
+    <hr>
+    obj2: {{ obj2 }}
+
+
     <button @click="changeTitle()">Změn title</button>
+    <button @click="changeObj()">Změn obj</button>
+    <button @click="changeObj2()">Změn obj 2</button>
+
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const title = ref('Composition API script');
 console.log('Čo je title 1:', title);
+
+let obj = ref({
+    firstName: "Jan",
+    lastName: "Novak"
+})
+
+let obj2 = reactive({
+    firstName: "Jan2",
+    lastName: "Novak2"
+})
 
 function greet() {
   console.log(title)
@@ -20,6 +38,18 @@ function changeTitle() {
   setTimeout(() => {
     title.value = 'Něco jiného'
     console.log('Čo je title 2:', title);
+  }, 1000)
+}
+
+function changeObj() {
+  setTimeout(() => {
+    obj.value.firstName = 'Honza'
+  }, 1000)
+}
+
+function changeObj2() {
+  setTimeout(() => {
+    obj2.firstName = 'Honza'
   }, 1000)
 }
 </script>
