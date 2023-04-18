2502f82517e0bc0580324a07257127957ff55ee9
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 09:24:33 +0200

    Update CompositionApiScript.vue

--- CompositionApiScript.vue
+++ CompositionApiScript.vue
@@ -1,17 +1,24 @@
 <template>
-    <div>
-        {{ title }}
-    </div>
+  <div>
+    {{ title }}
+
+    <button @click="changeTitle()">Změn title</button>
+  </div>
 </template>
 
 <script setup>
-  const title = 'Composition API';
+let title = 'Composition API script'
 
-  function greet() {
-        console.log(this.title);
-  }
+function greet() {
+  console.log(title)
+}
+
+function changeTitle() {
+  setTimeout(() => {
+    title = 'Něco jiného'
+    console.log('Čo je title:', title);
+  }, 1000)
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
