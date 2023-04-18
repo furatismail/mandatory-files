cffdaad44509af20deee5cfcdfc89690b9be3433
Furát Ismail <ismail.furat@gmail.com>
2023-04-18 11:20:45 +0200

    Update Child.vue

--- Child.vue
+++ Child.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="card">
         Potomek: {{ id }} {{ film }}
+        <button @click="getFilm({id, film})">VYBRAT FILM</button>
     </div>
 </template>
 
 <script setup>
+
 defineProps({
     id: String,
     film: {
@@ -12,12 +14,19 @@ defineProps({
         required: true
     }
 })
+
+const emits = defineEmits(['addFilm']);
+
+function getFilm(clickedFilm) {
+    console.log('clickedFilm: ', clickedFilm)
+    emits('addFilm', clickedFilm)
+}
+
 </script>
 
 <style scoped>
 .card {
     border: 1px solid #000;
-    background-color: yellow;
     padding: 20px;
     margin: 10px;
 }
