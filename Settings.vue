ebfacae49746b86b55a636d72e69c913ea2b4fcc
Furát Ismail <ismail.furat@gmail.com>
2023-04-19 09:32:02 +0200

    Add files via upload

--- Settings.vue
+++ Settings.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>   
-        <h1>Settings</h1>
+  <div>
+    <h1>Settings</h1>
+    <div v-for="i in [1, 2, 3, 4, 5, 6]" class="card" :key="i">
+      <router-link :to="{ name: 'SpecificSettings', params: { id: i } }">
+        Nastaveni pro {{ i }}
+      </router-link>
     </div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
-
+.card {
+  padding: 20px;
+  margin: 10px;
+  border: 1px solid aqua;
+}
 </style>
---
+++ SpecificSettings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Specific settings komponenta</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
