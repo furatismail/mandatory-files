251ba3de47517a18dfd2b40f8adb1ae4a3f56a3c
Furát Ismail <ismail.furat@gmail.com>
2023-04-19 11:02:38 +0200

    Update Home.vue

--- Home.vue
+++ Home.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+  <div>
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script setup>
+import FilmService from '../services/FilmService'
 
+FilmService.getFilms()
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => console.log(error))
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
