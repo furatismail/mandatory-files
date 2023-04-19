281313fc6119a79ff4c1a64dcce0d4b7fd4e148c
Furát Ismail <ismail.furat@gmail.com>
2023-04-19 09:42:33 +0200

    Update SpecificSettings.vue

--- SpecificSettings.vue
+++ SpecificSettings.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         <h1>Specific settings komponenta</h1>
+        {{ route.params?.id }}
+
     </div>
 </template>
 
 <script setup>
-
+import {useRoute} from 'vue-router';
+const route = useRoute();
+console.log(route);
 </script>
 
 <style scoped>
