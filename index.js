31c4ee12c8c05da18604fb8b65bcf12eed577f7d
Furát Ismail <ismail.furat@gmail.com>
2023-04-19 09:15:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Home from '../views/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Settings from '../views/Settings.vue'
 const routes = [
   {
     path: '',
@@ -11,11 +11,16 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import('../views/About.vue')
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   }
 ]
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router
